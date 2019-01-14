a6b1a3d117d4d5fc2d665c546478ea9e86c720be
rizkymez9 <rizkymez9@gmail.com>
2019-01-14 14:59:20 +0800

    Without share project with try someaccess web

---
+++ Object Repository/Page_Katalon Studio Simplify API We/p_Provide a complete Web testi.rs
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>p_Provide a complete Web testi</name>
+   <tag></tag>
+   <elementGuidId>94e55599-9602-4cb1-9de5-5eb9accf2c20</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>p</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Provide a complete Web testing solution</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;bs-navbar-main-collapse&quot;)/ul[@class=&quot;nav navbar-nav navbar-right&quot;]/li[@class=&quot;kat-dropdown dropdown menu-special desktop isactive&quot;]/div[@class=&quot;dropdown-menu&quot;]/div[@class=&quot;cbg&quot;]/div[@class=&quot;katalon&quot;]/div[@class=&quot;content&quot;]/a[@class=&quot;one-content&quot;]/p[1]</value>
+   </webElementProperties>
+</WebElementEntity>
